a2163720b58fea029b394c6dbceb0814f0ec4d23
g704116 <quang.nguyenminh41-ext@sagemcom.com>
2026-03-26 10:03:17 +0100

    Porting u-boot

---
+++ include/configs/qnm_bbb.h
@@ -0,0 +1,11 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/*
+ * QNM BBB configuration reuses the TI AM335x EVM/BeagleBone Black setup.
+ */
+
+#ifndef __CONFIG_QNM_BBB_H
+#define __CONFIG_QNM_BBB_H
+
+#include <configs/am335x_evm.h>
+
+#endif
