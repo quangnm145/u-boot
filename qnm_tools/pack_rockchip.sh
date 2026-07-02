948d146c5fbb11709b57bed12974061bac298645
qnm-pc <qnmhust.research@gmail.com>
2026-07-03 00:37:25 +0700

    Update uboot

--- qnm_tools/pack_rockchip.sh
+++ qnm_tools/pack_rockchip.sh
@@ -9,17 +9,25 @@ UBOOT_DIR=".."
 TOOLS_DIR="."
 BLOBS_DIR="rkbin_blobs"
 
-echo "=== Đóng gói idblock.bin (TPL + SPL) ==="
-TPL_BIN="${BLOBS_DIR}/rk3576_ddr_lp4_2112MHz_lp5_2736MHz_v1.08.bin"
+echo "=== Đóng gói idblock.bin bằng boot_merger ==="
 SPL_BIN="${UBOOT_DIR}/spl/u-boot-spl.bin"
 
-if [ ! -f "$TPL_BIN" ] || [ ! -f "$SPL_BIN" ]; then
-    echo "Lỗi: Không tìm thấy TPL hoặc SPL!"
+if [ ! -f "$SPL_BIN" ]; then
+    echo "Lỗi: Không tìm thấy SPL!"
     exit 1
 fi
 
-${UBOOT_DIR}/tools/mkimage -n rk3576 -T rksd -d ${TPL_BIN}:${SPL_BIN} ${UBOOT_DIR}/idblock.bin
-echo "Đã tạo thành công idblock.bin"
+# Cực kỳ quan trọng: Copy SPL vào chung thư mục để tránh lỗi đường dẫn tương đối của boot_merger
+cp "$SPL_BIN" "${BLOBS_DIR}/u-boot-spl.bin"
+
+./boot_merger RK3576_UBOOT.ini
+echo "Đã tạo thành công idblock.bin chuẩn Loader RK3576"
+
+echo "=== Cập nhật BL31 từ thư mục rkbin mới ==="
+BL31_ELF="../../rkbin/bin/rk35/rk3576_bl31_v1.24.elf"
+aarch64-linux-gnu-objcopy -O binary -j .text_pmusram $BL31_ELF rkbin_blobs/bl31_0x3fe70000.bin
+aarch64-linux-gnu-objcopy -O binary -j .ddr_m0_bin $BL31_ELF rkbin_blobs/bl31_0x400f0000.bin
+aarch64-linux-gnu-objcopy -O binary -j ro -j .sram.text -j .data -j .sram.data -j stacks -j .bss -j xlat_table -j coherent_ram $BL31_ELF rkbin_blobs/bl31_0x40060000.bin
 
 echo "=== Đóng gói uboot.img (FIT Image chứa ATF, TEE, U-Boot) ==="
 
